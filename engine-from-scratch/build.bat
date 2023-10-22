2c5b65a7acbec91fdf2d74d185ed9c17a30e6f1c
Loshan Sundaramoorthy <losh0001@gmail.com>
2023-10-22 21:57:30 +0100

    Testing and setting up environment

--- engine-from-scratch/build.bat
+++ engine-from-scratch/build.bat
@@ -1,4 +1,4 @@
 set files=src\glad.c src\main.c 
-set libs=D:\GameEngine\lib\SDL2main.lib D:\GameEngine\lib\SDL2.lib D:\GameEngine\lib\freetype.lib
+set libs=D:\game_engine\lib\SDL2main.lib D:\game_engine\lib\SDL2.lib D:\game_engine\lib\freetype.lib
 
-CL /Zi /I D:\GameEngine\include %files% /link %libs% /OUT:game.exe 
+CL /Zi /I D:\game_engine\include %files% /link %libs% /OUT:game.exe 
